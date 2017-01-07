0bd93b10711bb5a6a9dce29b48d24048229406ba
Martynas Kadiša <Martynas Kadia>
2017-01-07 20:36:43 +0200

    Optimize for production

--- webpack.server.config.js
+++ webpack.server.config.js
@@ -5,6 +5,7 @@ var webpack = require('webpack')
 var nodeExternals = require('webpack-node-externals')
 
 module.exports = {
+  devtool: 'source-map',
   entry: [
     './server/handlers/renderHandler.js'
   ],
